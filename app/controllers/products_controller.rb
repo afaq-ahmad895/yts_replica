ee967bfaac85333281d641c59d48e5085e52f13d
Your Name <you@example.com>
2023-10-16 22:36:44 +0500

    [BB-9] the goal was to remove the created_at and updated_at fields from the JSON response

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,11 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       format.html
-     format.json { render json: @products }
+   #  format.json { render json: @products }
+   format.json do
+
+     render json: @products.to_json(only: [:id, :name, :description])
+   end
     end
 
   end
