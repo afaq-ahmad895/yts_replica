dadb6331803fb0a6a8e84bce58a9922f7419f86d
Your Name <you@example.com>
2023-10-25 11:44:21 +0500

    [BB-12][BB-15] ruby only : in process => all data in one loop .

--- public/yml_test_cases/nested_hash_array_mix/nested_hash_array_yml_test.rb
+++ public/yml_test_cases/nested_hash_array_mix/nested_hash_array_yml_test.rb
@@ -3,27 +3,83 @@ require "awesome_print"
 require 'yaml'
 # binding.pry
 data= YAML.load_file('nested_hash_array.yml')
-c = [] 
-
+teachers1 = []
+teachers2 = []
 data['schools'].each_with_index do |school, index|
 
-  students = []
-  school['classes'].each do |klass|
-    students += klass['students']
+teachers_classes = {}
+
+puts "#{index + 1}#{index == 0 ? 'st' : 'nd'} school name: #{school['name']}"
+
+sum = 0
+school['classes'].each do |klass|
+  if klass['students'].is_a?(Array)
+    sum += klass['students'].length
+  end
+end
+
+#total students in wach class.
+puts "List of Classes (total #{sum} students):"
+school['classes'].each do |klass|
+  subject = klass['subject']
+  students = klass['students'].length
+  
+  puts "- #{subject} (#{students})"
+
+
+
+    teacher = klass['teacher']
+    subject = klass['subject']
+    
+    if teachers_classes[teacher]
+      teachers_classes[teacher] << subject
+    else
+      teachers_classes[teacher] = [subject]
+    end
   end
 
- c += students 
+  teachers_classes.each do |teacher, classes|
+    if classes.length > 1
+      puts "Mr.#{teacher} is Teaching: [#{classes.join(', ')}]"
+    end
 
- puts "#{index + 1}#{index == 0 ? 'st' : 'nd'} school name: #{school['name']}"
+end
 
-  puts "List of students(total #{students.length})->unique(#{students.uniq.length})"
+
+#unique subjects offered in one school but not in other.
+  school_subjects = school['classes'].map { |klass| klass['subject'] }
+
+  unique_subjects = school_subjects - (index == 0 ? data['schools'][1]['classes'].map { |klass| klass['subject'] }
+  : data['schools'][0]['classes'].map { |klass| klass['subject'] })
+
+  puts "subjects only offered in #{school['name']} school not in other:"
+   unique_subjects.each do |subject|
+     puts "- #{subject}"
+  end
+
 
 
 end
-puts "total students of both school: (#{c.length} students)->unique (#{c.uniq.length})"
 
 
-puts "==================================================================================================="
+#Commmon tacher linkedd more than one scchool
+data['schools'][0]['classes'].each do |klass|
+  teachers1 << klass['teacher']
+end
+
+data['schools'][1]['classes'].each do |klass|
+  teachers2 << klass['teacher']
+end
+
+common_teacher = teachers1 & teachers2
+
+if common_teacher.any?
+  puts "List of Teachers Teaching in more then One School:"
+  puts "- #{common_teacher.first}"
+else
+  puts "No common teacher found."
+end
+
 
 
 
