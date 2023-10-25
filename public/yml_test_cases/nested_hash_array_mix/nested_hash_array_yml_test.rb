cdeb0b27cb8b31261af972539d71e448b1923465
Your Name <you@example.com>
2023-10-25 16:00:53 +0500

    [BB-12][BB-15] ruby only : in progress => 1.

--- public/yml_test_cases/nested_hash_array_mix/nested_hash_array_yml_test.rb
+++ public/yml_test_cases/nested_hash_array_mix/nested_hash_array_yml_test.rb
@@ -3,72 +3,26 @@ require "awesome_print"
 require 'yaml'
 
 data = YAML.load_file('nested_hash_array.yml')
-c = []
-teachers1 = []
-teachers2 = []
 
-data['schools'].each_with_index do |school, index|
-  teachers_classes = {}
-  # Extract the students and classes for the current school
-  students = school['classes'].flat_map { |klass| klass['students'] }
-  classes = school['classes']
-
-  puts "#{index + 1}#{index == 0 ? 'st' : 'nd'} school name: #{school['name']}"
-
-  # Print information for each class
-  puts "List of Classes (total #{classes.sum { |klass| klass['students'].length }} students):"
-  classes.each do |klass|
-    subject = klass['subject']
-    students = klass['students'].length
-    puts "- #{subject} (#{students})"
+data['schools'].each_with_index do |school,index|
+    puts "#{index + 1}#{index == 0 ? 'st' : 'nd'} school name: #{school['name']}"
+    sum = 0
+    school['classes'].each do |klass|
+    # Track total students
+    if klass['students'].is_a?(Array)
+    sum += klass['students'].length
   end
+#total students in each class.
+puts "List of Classes (total #{sum} students):"
+subject = klass['subject']
+students = klass['students'].length
+  
+puts "- #{subject} (#{students})"
 
-  # Teachers teaching more than one class
-  classes.each do |klass|
-    teacher = klass['teacher']
-    subject = klass['subject']
-
-    if teachers_classes[teacher]
-      teachers_classes[teacher] << subject
-    else
-      teachers_classes[teacher] = [subject]
-    end
-  end
-
-  teachers_classes.each do |teacher, classes|
-    if classes.length > 1
-      puts "Mr.#{teacher} is Teaching: [#{classes.join(', ')}]"
-    end
-  end
-
-
-  #unique subjects offered in one school but not in other.
-  school_subjects = school['classes'].map { |klass| klass['subject'] }
-
-  unique_subjects = school_subjects - (index == 0 ? data['schools'][1]['classes'].map { |klass| klass['subject'] }
-  : data['schools'][0]['classes'].map { |klass| klass['subject'] })
-
-  puts "subjects only offered in #{school['name']} school not in other:"
-   unique_subjects.each do |subject|
-     puts "- #{subject}"
-  end
 end
 
 
-#Commmon tacher linkedd more than one scchool
-data['schools'][0]['classes'].each do |klass|
-  teachers1 << klass['teacher']
-end
 
-data['schools'][1]['classes'].each do |klass|
-  teachers2 << klass['teacher']
-end
 
-common_teacher = teachers1 & teachers2
 
-if common_teacher.any?
-  puts "List of Teachers Teaching in more then One School:"
-  puts "- #{common_teacher.first}"
-else
-  puts "No common teacher found."
 end
