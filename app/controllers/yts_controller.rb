285a903fd9c658de7d461d0e1fcd64194d97fa9f
Your Name <you@example.com>
2023-10-29 17:19:12 +0500

    [BB-9]: return HTML or JSON data based on the incoming request format of yts_replica

--- app/controllers/yts_controller.rb
+++ app/controllers/yts_controller.rb
@@ -2,19 +2,6 @@ class YtsController < ApplicationController
   def index
     @movie_rows = YAML.load_file(Rails.root.join('config', 'movies.yml'))
 
-
-    @movie_rows.to_json do |movie|
-      
-      binding.pry
-
-
-      # {
-      #   name: movie['name'],
-      #   year: movie['year'],
-      #   rating: movie['rating']
-      # }
-    end
-
     respond_to do |format|
       format.html
       format.json { render json: @movie_rows }
--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,28 @@ Rails.application.routes.draw do
   get 'yts/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+# def index
+#   @movie_rows = YAML.load_file(Rails.root.join('config', 'movies.yml'))
+#
+#   respond_to do |format|
+#     format.html
+#     format.json do
+#       json_data = {}
+#       @movie_rows.each_with_index do |row, index|
+#         row_key = "row_#{index + 1}"
+#         json_data[row_key] = row.map do |movie|
+#           {
+#             name: movie['name'],
+#             rating: movie['rating'],
+#             year: movie['year']
+#           }
+#         end
+#       end
+#       render json: json_data
+#     end
+#   end
+# end
