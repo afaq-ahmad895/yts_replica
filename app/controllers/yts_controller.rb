1a6b3dac454227b82090e100aadac580f4788be5
Your Name <you@example.com>
2023-10-29 18:15:57 +0500

    [BB-9]:show only name,rating and year, return HTML or JSON request, yts_replica

--- app/controllers/yts_controller.rb
+++ app/controllers/yts_controller.rb
@@ -1,11 +1,21 @@
 class YtsController < ApplicationController
-  def index
-    @movie_rows = YAML.load_file(Rails.root.join('config', 'movies.yml'))
+def index
+  @movie_rows = YAML.load_file(Rails.root.join('config', 'movies.yml'))
 
-    respond_to do |format|
-      format.html
-      format.json { render json: @movie_rows }
+  respond_to do |format|
+    format.html
+    format.json do
+      json_data = @movie_rows.map do |movies|
+        movies.map do |movie|
+          {
+            name: movie['name'],
+            rating: movie['rating'],
+            year: movie['year']
+          }
+        end
+      end
+      render json: json_data
     end
   end
-
+end
 end
