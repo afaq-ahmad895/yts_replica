36939cf55b3b0ffc554021107cb4957c5d4179ff
Your Name <you@example.com>
2023-10-30 11:16:34 +0500

    [BB-9]:show all movie data return HTML or JSON request, yts_replica

--- app/controllers/yts_controller.rb
+++ app/controllers/yts_controller.rb
@@ -1,16 +1,22 @@
 class YtsController < ApplicationController
+
 def index
   @movie_rows = YAML.load_file(Rails.root.join('config', 'movies.yml'))
 
   respond_to do |format|
     format.html
     format.json do
-      json_data = @movie_rows.map do |movies|
-        movies.map do |movie|
+      json_data = {}
+      @movie_rows.each_with_index do |row, index|
+        row_key = "row_#{index + 1}"
+        json_data[row_key] = row.map do |movie|
           {
             name: movie['name'],
             rating: movie['rating'],
-            year: movie['year']
+            year: movie['year'],
+            drama: movie['drama'],
+            action: movie['action'],
+            image: movie['image']
           }
         end
       end
@@ -19,3 +25,10 @@ def index
   end
 end
 end
+
+
+
+
+
+
+
