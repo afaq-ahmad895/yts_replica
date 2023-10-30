fd7529f75bf956f5921c4fd23090934adf85a521
Your Name <you@example.com>
2023-10-30 12:36:33 +0500

    [BB-9]:show specific data using .each loop, return HTML or JSON request, yts_replica

--- app/controllers/yts_controller.rb
+++ app/controllers/yts_controller.rb
@@ -1,29 +1,28 @@
 class YtsController < ApplicationController
+  def index
+    @movie_rows = YAML.load_file(Rails.root.join('config', 'movies.yml'))
 
-def index
-  @movie_rows = YAML.load_file(Rails.root.join('config', 'movies.yml'))
+    json_data = {}
+    @movie_rows.each_with_index do |row, index|
+      row_key = "row_#{index + 1}"
 
-  respond_to do |format|
-    format.html
-    format.json do
-      json_data = {}
-      @movie_rows.each_with_index do |row, index|
-        row_key = "row_#{index + 1}"
-        json_data[row_key] = row.map do |movie|
-          {
-            name: movie['name'],
-            rating: movie['rating'],
-            year: movie['year'],
-            drama: movie['drama'],
-            action: movie['action'],
-            image: movie['image']
-          }
-        end
+      json_data[row_key] = []
+      row.each do |movie|
+        json_data[row_key] << {
+          name: movie['name'],
+          rating: movie['rating'],
+          year: movie['year']
+        }
       end
-      render json: json_data
+    end
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: json_data
+      end
     end
   end
-end
 end
 
 
