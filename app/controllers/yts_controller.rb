c8f651dc404235e845dd0f0c65eac5384d434007
Your Name <you@example.com>
2023-11-21 16:57:30 +0500

    [BB-8] call search api and show their data on index

--- app/controllers/yts_controller.rb
+++ app/controllers/yts_controller.rb
@@ -88,20 +88,20 @@ end
 
 
   def search
-    # @movie_id = params[:movie_id]
-    response = HTTParty.get('https://imdb-top-100-movies.p.rapidapi.com/top22', headers: {
-      'X-RapidAPI-Host' => 'imdb-top-100-movies.p.rapidapi.com',
+
+    response = HTTParty.get('https://movies-api14.p.rapidapi.com/search?query=breaking%20bad', headers: {
+      'X-RapidAPI-Host' => 'movies-api14.p.rapidapi.com',
       'X-RapidAPI-Key' => '7a15c90dfbmsh27f1e2e871c0f21p1c7c11jsn784ee34a2303'
     })
-    @movie_detail = JSON.parse(response.body)
+    @movie_detail = JSON.parse(response.body) ['contents']
 
+    # @movie_id = params[:movie_id]
     # response = HTTParty.get("https://imdb-top-100-movies.p.rapidapi.com/movieDetails/#{movie_id}", headers: {
     #   'X-RapidAPI-Host' => 'imdb-top-100-movies.p.rapidapi.com',
     #   'X-RapidAPI-Key' => '7a15c90dfbmsh27f1e2e871c0f21p1c7c11jsn784ee34a2303'
     # })
     #
     # @movie_detail = JSON.parse(response.body)
-
   end
   
 
