2622f40c419eb1939c001bd51e36f9e823b916fa
Your Name <you@example.com>
2024-01-05 11:47:16 +0500

    [BB-35] try to save api record into database not working

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,13 +1,6 @@
 class ApisController < ApplicationController
+  require 'pry'
 
-  # def create
-  #   @movie = Api.new(movie_params)
-  #   if @movie.save
-  #     flash[:notice] = "movie created."
-  #   else
-  #     flash[:alert] = "movie not created."
-  #   end
-  # end
 
   def index
     response = HTTParty.get('https://movies-api14.p.rapidapi.com/movies', headers: {
@@ -15,8 +8,18 @@ class ApisController < ApplicationController
       'X-RapidAPI-Key' => '562c0c79a0msha9b8db4de63e59dp131a2djsna425bbbdbb01'
     })
     movies_data = JSON.parse(response.body)['movies']
-    movies_data.each do |movie_data|
-      Api.create(movie_data)
+    # movies_data.each do |movie_data|
+    #   Api.create(movie_data)
+    # end
+    binding.pry
+    movies_data.each do |movie|
+      a = Api.create(
+        title: movie['title'],
+        release_date: movie['release_date'],
+        genres: movie['genres'],
+        rating: movie['rating'],
+        poster_path: movie['poster_path'],
+      )
     end
   end
 
